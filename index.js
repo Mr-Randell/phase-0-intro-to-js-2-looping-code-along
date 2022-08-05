f5576cbbd1d418dc76e7fa4e5d3896532429c29f
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-08-05 15:04:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+/*for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}*/
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+let duplicate = [];
+
+function writeCards(names, eventName = "surprise!") {
+    for (let i = 0; i < names.length; i++) {
+        duplicate.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+        //debugger;
+    }
+
+return duplicate;
+}
+
+function countDown() {
+    let count = 10;
+    while (count >= 0) {
+        console.log(count--)
+    }
+}
+
+countDown();
